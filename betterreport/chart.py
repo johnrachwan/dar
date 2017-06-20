aaef4169f11277a28f3f1a41b907b048e7751c82
johnrachwan <john.rachwan@dar.com>
2017-06-20 09:07:00 +0300

    Add files via upload

---
+++ betterreport/chart.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Not Clicked', 'Clicked'
+sizes = [15, 30]
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+#plt.show()
+plt.savefig("john", dpi=150, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=True, bbox_inches=None, pad_inches=0.1,
+        frameon=None)
